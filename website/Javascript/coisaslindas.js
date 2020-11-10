2105958e0ce3137e68ad04c6e3f9a62306eca8a6
Tiago Azevedo <tiagomina.azevedo@upskill.pt>
2020-11-10 17:04:01 +0000

    Comment JS updates

--- website/Javascript/coisaslindas.js
+++ website/Javascript/coisaslindas.js
@@ -2,4 +2,15 @@
 function commentTemplate() {}
 
 // Get info from Forms
-function getName() {}
+function getName() {
+  console.log("Olá!");
+  document.querySelector("#text_name").innerHTML = document.querySelector(
+    "#myName"
+  ).value;
+}
+
+function getComment() {
+  document.querySelector("#text_comment").innerHTML = document.querySelector(
+    "#myComment"
+  ).value;
+}
